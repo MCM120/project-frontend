b97c67226aff6cd9b3bc3baf9d2e3ba2f7c0c22a
MCM120 <mariana.castellanos@wompi.com>
2025-06-24 13:24:24 -0500

    feat: improve js

--- js/main.js
+++ js/main.js
@@ -1,43 +1,35 @@
-/*nav*/
+/* NAV */
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburguer = document.querySelector('#hamburguer-icon');
 
-const hamburguer = document.querySelector('#hamburguer-icon')
-const options = document.querySelector('#options')
-const navMenu = document.querySelector('#menu-options')
-
-hamburguer.addEventListener('click', () => {
-    options.classList.toggle('show')
-    document.body.classList.toggle('no-scroll');
-    navMenu.classList.toggle('show')
-    hamburguer.classList.toggle('show')
-})
+    hamburguer.addEventListener('click', () => {
+        ['options', 'menu-options', 'hamburguer-icon'].forEach(id => {
+            document.getElementById(id).classList.toggle('show');
+        });
+        document.body.classList.toggle('no-scroll');
+    });
 
 
-/*""carousel""*/
+    /* CAROUSEL */
+    const slides = document.querySelectorAll('.cards');
+    const btns = document.querySelectorAll('.btn');
+    let currentSlide = 1;
 
-var slides = document.querySelectorAll('.cards');
-var btns = document.querySelectorAll('.btn');
-let currentSlide = 1;
+    const clearActiveClasses = () => {
+        slides.forEach(slide => slide.classList.remove('active'));
+        btns.forEach(btn => {
+            btn.classList.remove('active');
+            btn.classList.add('regular-opt');
+        });
+    };
 
-var nav = function (manual) {
-    slides.forEach((slide) => {
-        slide.classList.remove('active')
-
-        btns.forEach((btn) => {
-            btn.classList.remove('active')
-            btn.classList.add('regular-opt')
-        })
-    })
-
-    slides[manual].classList.add('active');
-    btns[manual].classList.add('active');
-    btns[manual].classList.remove('regular-opt');
-}
-
-btns.forEach((btn, i) => {
-    btn.addEventListener("click", () => {
-        nav(i);
-        currentSlide = i;
-    })
+    btns.forEach((btn, i) => {
+        btn.addEventListener("click", () => {
+            clearActiveClasses();
+            slides[i].classList.add('active');
+            btn.classList.add('active');
+            btn.classList.remove('regular-opt');
+            currentSlide = i;
+        });
+    });
 });
-
-console.log("empanada")
